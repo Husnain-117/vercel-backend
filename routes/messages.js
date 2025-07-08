51fc2a4ab52bbb0a91d355de52c6d3389322e782
Husnain Akram <husnainakram525@gmail.com>
2025-07-08 15:59:24 +0500

    Fix: ensure server starts in production for Railway

--- routes/messages.js
+++ routes/messages.js
@@ -1,18 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// CORS middleware for Vercel serverless
-router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://fast-connect-three.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-});
+
 const textController = require('../controllers/textController');
 const { authenticate } = require('../middleware/authenticate');
 
